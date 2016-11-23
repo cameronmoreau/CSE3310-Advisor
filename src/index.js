8206ea56149151cc35a59277e7ab795aa47e3a03
Cameron Moreau <cameron.cm6@gmail.com>
2016-11-23 13:50:10 -0600

    Added bootstrap + material design bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-material-design/dist/css/bootstrap-material-design.min.css';
+
+import './css/index.css';
 
 ReactDOM.render(
   <App />,
